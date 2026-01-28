2ea82c1702e874fa866b4d294a16152a6b51d329
Landy Ortiz <landy.onz@gmail.com>
2026-01-27 19:19:12 -0600

    commit inicial

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function Mensaje(): string{
+        return "Hola desde el controlador de PostController";
+    }
+
+    public function About($param=null,$nombre=null){
+        $datos=['parametro'=>$param,'nombre'=>$nombre];
+        return view('about',$datos);
+        //return view('about',compact('param','nombre'));
+    }
+
+    public function Contacto(){
+        return view('contacto',['mensaje'=>'Esto es un mensaje']);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PrincipalController;
+
+/*Route::get('/', function () {
+    return view('welcome');
+    
+});*/
+
+Route::get('/hello',HomeController::class);//index
+Route::get('post/mensaje',[PostController::class,'Mensaje']);//metodo
